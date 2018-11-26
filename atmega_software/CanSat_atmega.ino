78e1e0e08cb4aaaf57e969f4a78f1df960ea9e16
--global <matteusz.lichota@gmail.com>
2018-11-26 16:22:16 +0100

    restructured some more

--- atmega_software/CanSat_atmega.ino
+++ atmega_software/CanSat_atmega.ino
@@ -9,6 +9,24 @@
 uint8_t mission_phase = PRE_LAUNCH_PHASE;
 bool status_acknowledged = false;
 
+/*
+bit | device
+0   raspberry
+1   mpu9250
+2   camera
+3   -------
+4   bme280
+5   LoRa
+6   gps
+7   --------
+*/
+uint8_t devices_status = 0b00000000; //initially no devices working is assumed
+
+void set_status_bit(uint8_t bit, bool value){
+    devices_status |= 0b11111111 & value << bit;
+}
+
+
 //Raspberry Pi
 #include <AltSoftSerial.h>
 AltSoftSerial altSerial;
@@ -52,6 +70,15 @@ uint8_t lora_buffer[255] = {0};
 uint8_t lora_buffer_ptr = 0;
 bool lora_ok = false;
 
+long last_packet_transmission = 0; //the value of millis() of last transmission
+#define MAX_PACKET_SEPARATION 950 //maximum time between packets in milliseconds
+#define STATUS_REPEAT_PERIOD 10000 //time between status retransmissions in phase 1
+
+//possible commands from ground station (via radio)
+#define ACTION_ACK 0x01 //acknowledge status
+#define ACTION_FLY 0x02 //enable flight mode
+#define ACTION_CMD 0x03 //a command to raspberry pi
+
 /**
  * This function sets up the LoRa module with settings from the top of the file
  * It returns 0 on success and 1 on failure.
@@ -70,16 +97,18 @@ bool initLora()
     LoRa.setSignalBandwidth(LORA_BANDWIDTH);
     LoRa.setupInterrupts(onTxDone, onRxDone);
     LoRa.receive();
+    debug(" LoRa init ok");
     return 1;
 }
 
 /**
- * This function sends the first <<bytes>> of the tx_buffer.
+ * This function sends the first <<bytes>> bytes of <<buffer>> via lora
  */
-void sendBuffer(uint8_t bytes)
+void sendBuffer(uint8_t* buffer, uint8_t bytes)
 {
+    last_packet_transmission = millis();
     LoRa.beginPacket();
-    LoRa.write(lora_buffer, bytes);
+    LoRa.write(buffer, bytes);
     LoRa.endPacket();
 }
 
@@ -90,9 +119,7 @@ void onTxDone()
 {
 }
 
-#define ACTION_ACK 0x01
-#define ACTION_FLY 0x02
-#define ACTION_CMD 0x03
+
 
 /**
  * This function is executed when a full packet is received, number of bytes
@@ -102,7 +129,7 @@ void onTxDone()
 void onRxDone(int packet_size, bool crc_error)
 {
     //check if the packet is for us
-    if (packet_size >= 3 && LoRa.read() == 0x08 && LoRa.read() == 0x07) 
+    if (packet_size >= 3 && LoRa.read() == 0x08 && LoRa.read() == 0x07)
     {
         uint8_t action = LoRa.read();
         switch (action)
@@ -124,25 +151,30 @@ void onRxDone(int packet_size, bool crc_error)
     }
 }
 
-void parse_lora_packet(){
-    while(LoRa.available()){
+/**
+ * Copies all data from lora rx buffer to local lora_buffer
+ */
+void parse_lora_packet()
+{
+    while (LoRa.available())
+    {
         lora_buffer[lora_buffer_ptr] = LoRa.read();
-        lora_buffer_ptr+=1;
+        lora_buffer_ptr += 1;
     }
 }
 
-void send_command_to_pi(uint8_t* command, uint8_t length)
+/**
+ * Sends the supplied command with $C prefix
+ */
+void send_command_to_pi(uint8_t *command, uint8_t length)
 {
-    uint8_t ptr = 0;
     PI_SERIAL.write("$C");
-    for (ptr; ptr < length; ++ptr)
-    {
-        PI_SERIAL.write(command[ptr]);
-    }
+    PI_SERIAL.write(command, length);
 }
 
 //GPS
 #define GPS_SERIAL //gps serial
+
 /**
  * Initializes the gps receiver serial port
  */
@@ -150,6 +182,7 @@ void initGPS()
 {
     //TODO
 }
+
 /**
  * Switches gps to low power mode
  */
@@ -166,14 +199,13 @@ void setGPSMaxPower()
     //TODO
 }
 
-uint8_t pi_status = 0;
 #define PI_OK_STATUS 0b00000111 //two working devices and pi booted (00000111)
 #define PI_FAILED_BOOT 0b00000000
 
 /**
- * does nothing basically
+ * waits for activity on PI_SERIAL, returns error on timeout
  */
-uint8_t get_pi_status()
+void boot_pi()
 {
     long task_started = millis();
     while (!PI_SERIAL.available() && millis() - task_started < long(PI_BOOT_TIMEOUT) * 1000)
@@ -181,15 +213,30 @@ uint8_t get_pi_status()
     if (millis() - task_started >= long(PI_BOOT_TIMEOUT) * 1000)
     {
         debug(" PI boot timeout");
-        return PI_FAILED_BOOT;
+        devices_status &= 0b00011111;
     }
     else
     {
         debug(" PI booted ok");
-        //return read_from_pi();
+        delay(20); //give pi time to write whole status
+        if (PI_SERIAL.read() == '$' && PI_SERIAL.read() == 'S'){
+            uint8_t status = PI_SERIAL.read();
+            //change first 4 bits of devices_status without changing the last 4
+            devices_status |= status & 0b11110000;
+            devices_status &= status | 0b00001111;
+        }else{
+            debug("message from pi is broke");
+            devices_status &= 0b00011111; 
+            //get status again
+        }
+        
+
     }
 }
 
+/**
+ * Can be treated as "enter pre launch phase" function
+ */
 void setup()
 {
     DEBUG_SERIAL.begin(38400);
@@ -198,8 +245,8 @@ void setup()
     switch_pi_rail(HIGH);
     initGPS();
     setGPSLowPower();
-    lora_ok = initLora();
-    pi_status = get_pi_status();
+    set_status_bit(6, initLora());
+    boot_pi();
 }
 
 /**
@@ -252,8 +299,7 @@ void loop()
 }
 
 /**
- * Executed untill launch command is received or a big altitude change is detected,
- * switches to mission_phase 1 afterwards.
+ * Sends the status untill acknowledgement is received, checks for launch symptoms
  */
 void pre_launch_loop()
 {
@@ -261,14 +307,23 @@ void pre_launch_loop()
 
     //If ACK hasn’t been received & 10s have passed from last time: AT sends status
     //(handled by the lora interrupt function)
+    if (!status_acknowledged && millis()-last_packet_transmission > STATUS_REPEAT_PERIOD){
+        uint8_t packet[3] = {0x07, 0x08, devices_status};
+        sendBuffer(packet, 3);
+    }
 
     //If commands are received ($C) they are passed to and executed on PI, and the output is returned via LoRa
     //((handled by the lora interrupt function)
 
     //get gps altitude, compare with initial, if the signal is good and the altitude is over
     //200m greater initiate flight phase
+
 }
 
+/**
+ * Enables the pi and sets up everything at max efficiency for the flight,
+ * switches mission_phase to POST_FLIGHT_PHASE
+ */
 void switch_to_flight_phase()
 {
     //AT enables 5V rail, PI is booted
@@ -276,24 +331,74 @@ void switch_to_flight_phase()
 
     //AT enables full efficiency on GPS and BME
     setGPSMaxPower();
-
-    //AT sends “launch confirmation” to ground
-    memcpy(lora_buffer, "$L", 2);
-    //lora_buffer[2] = pi status
-    sendBuffer(3);
+    //setBMEMaxPower(); - to be implemented
     
+
     //AT waits for PI to boot
-    pi_status = get_pi_status();
+    boot_pi();
 
-    //PI performs boot routine[1]
-    
+    uint8_t message[4] = {0x07, 0x08, 'L'};
+    message[4] = devices_status;
+    //AT sends “launch confirmation” to ground
+    sendBuffer(message, 4);
+
     //AT sends “flight mode ($F)” to PI
-    send_command_to_pi((uint8_t*)"$F", 2);
+    send_command_to_pi((uint8_t *)"$F", 2);
 
+    mission_phase = FLIGHT_PHASE;
 }
 
+
+/**
+ * Sends gps data to pi, gets back imu data and status
+ */
+void exchange_data_with_pi(){
+    //TODO
+    //first send gps data
+    //then receive imu and status
+}
+
+/**
+ * Sends all of available data to the ground station
+ */
+void send_data_to_gs(){
+    //TODO
+}
+
+
+/**
+ * Executed during the flight phase
+ */
 void flight_loop()
 {
+    //Try to get data from BME (change status if failed) - as often as possible
+    //parse_bme_data();
+
+    //Read GPS data
+    //process_gps();
+    
+    //Send converted gps data to PI, get data in return
+    exchange_data_with_pi();
+
+
+    //if time from last send is > 0.9s, send new packet
+    if (millis()-last_packet_transmission > MAX_PACKET_SEPARATION){
+        send_data_to_gs();
+    }
+    
+    //If “landing” recieved from PI: break loop
+    //(handled by on_rx_done)
+}
+
+/**
+ * Switches everything to low power consumption, and changes the mission
+ * phase to POST_FLIGHT_PHASE
+ */
+void switch_to_post_flight_phase()
+{
+    mission_phase = POST_FLIGHT_PHASE;
+    LoRa.setSpreadingFactor(12);
+    setGPSLowPower();
     //TODO
 }
 
@@ -304,6 +409,7 @@ void flight_loop()
 void post_flight_loop()
 {
     //go to sleep for 10 seconds
+    delay(10000);
 
     //send a packet with location at SF12
 }
