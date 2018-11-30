6895ce52123f497f40f721b7ab6c1c48aaf63c49
--global <matteusz.lichota@gmail.com>
2018-11-30 16:44:23 +0100

    minor changes

---
+++ atmega_software/CanSat_atmega/GPS_MODULE.h
@@ -0,0 +1,20 @@
+#ifndef GPS_MODULE_H
+#define GPS_MODULE_H
+
+class GPS_MODULE{
+    public:
+    GPS_MODULE(){} //constructor
+    ~GPS_MODULE(){} //destructor
+    bool begin(){
+    }
+    bool set_max_power(){
+        //TODO
+    }
+
+
+    bool set_low_power(){
+        //TODO
+    }
+};
+
+#endif //GPS_H
